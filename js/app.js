0d7fb40086536ab0e1ed4ff24569ed817a85b47f
Suttipong_Mek <suttipong.mek@gmail.com>
2020-10-19 04:07:37 +0700

    end

--- js/app.js
+++ js/app.js
@@ -27,6 +27,7 @@ document.addEventListener('init', function (event) {
 
 
   if (page.id === "home") {
+
     db.collection("movies").get().then(function (querySnapshot) {
       querySnapshot.forEach(function (doc) {
         // doc.data() is never undefined for query doc snapshots
@@ -42,21 +43,33 @@ document.addEventListener('init', function (event) {
 
   }
 
-  //test----------------------------
-  // if (page.id === "movie") {
-  //   db.collection("movies").get().then(function (querySnapshot) {
-  //     querySnapshot.forEach(function (doc) {
-  //       // doc.data() is never undefined for query doc snapshots
-  //       console.log(doc.id, " => ", doc.data());
-  //     var item =`<ons-carousel-item modifier="nodivider" id="Recent Movie 1" class="movie_cover" onclick="openMovieDetails(this.id)">
-  //     <img src="${doc.data().posterURL}" style="width:100%;"> </ons-carousel-item>`
-  //       var onsItem = document.createElement('ons-carousel-item');
-  //       onsItem.innerHTML = item;
-  //       page.querySelector('#carousel').appendChild(onsItem);
-  //     });
-  //   });
 
-  // }
+  //test----------------------------
+  if (page.id === "movie_details") {
+    db.collection("movies").get().then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        var item = `<div>
+        <div style="text-align: center;"> <img src="${doc.data().posterURL}" style="width:100%;"></div>
+        <div class="card-doby">
+           <h4 class="card-title">${doc.data().title} - ${doc.data().year}</h4>
+           <p class="card-text">${doc.data().detail}</p>
+           
+      </div>
+      <center>
+      <div>Trailer</div>
+                  <iframe src="${doc.data().trailer}" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen></iframe>
+                  <center></div>`
+        var onsItem = document.createElement('ons-carousel-item');
+        onsItem.innerHTML = item;
+        page.querySelector('#Detail').appendChild(onsItem);
+      })
+    });
+
+  }
   //test-----------------------------------------
 
   if (page.id === "signin") {
@@ -101,21 +114,19 @@ document.addEventListener('init', function(event) {
       document.querySelector('#menu').open();
     };
 
-  } else if (page.id === 'movie_details') {
-    page.querySelector('#movie_title').innerHTML = page.data.title;
+  
   }
+  
 });
 
 function openMovieDetails(id) {
   document.querySelector('#myNavigator').pushPage('movie_details.html', {data: {title: id}});
 }
 
-// function openHome() {
-//   document.querySelector('#myNavigator').pushPage('home_splitter.html');
-// }
 
 function goBack() {
   document.querySelector('#menu').close().then(function() {
     document.querySelector('#myNavigator').popPage()
   });
 }
+
